dfc031cb12046011b76acf039873620a117c83bb
Kateryna-4yka <kotasyri@gmail.com>
2025-05-05 22:10:41 +0300

    2

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB = './src/db/db.json';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const meta = fileURLToPath(import.meta.url);
+const dir = path.dirname(meta);
+
+export const PATH_DB = path.resolve(dir, '../db/db.json');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { readContacts } from './utils/readContacts.js';
 // import {createFakeContact} from './utils/createFakeContact.js';
 // import {writeContacts} from './utils/writeContacts.js';
-// import {generateContacts} from './scripts/generateContacts.js';
-// generateContacts(5);
+// import {PATH_DB} from './constants/contacts.js';
+// // generateContacts(5);
+// console.log(PATH_DB);
